cd4ab05a9f2de132d5b4c662344ba36ef8d15ebc
rhdmswls12 <rhgusdnr1293@naver.com>
2024-01-15 18:13:56 +0900

    배열의 깊은 복사

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
-import cloneDeep from 'lodash/cloneDeep'
+import { cloneDeep } from "lodash"
 
-
-const a = {x: {y: 1}}
+const a = [[1, 2] , [3]]
 const b = cloneDeep(a) // 깊은 복사
 
-b.x.y = 2
+b[0][0] = 4
 
-console.log(b) // 2 
-console.log(a) // 1
-
-const arr1 = [1, 2, 3]
-const arr2 = arr1.concat([]) // 얕은 복사
-const arr3 = [...arr1] // 얕은 복사
-
-arr2[0] = 4
-arr3[0] = 5
-
-console.log(arr3) // [5, 2, 3]
-console.log(arr2) // [4, 2, 3]
-console.log(arr1) // [1, 2, 3]
+console.log(b) // [[4, 2], [3]]
+console.log(a) // [[1, 2], [3]]
